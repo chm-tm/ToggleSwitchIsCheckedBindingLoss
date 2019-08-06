5223e4a6134213f83764050bc96b29b7cd20ffa3
Christian Marrocco <chm@triamec.com>
2019-08-06 10:12:38 +0200

    Work around [#2852](https://github.com/MahApps/MahApps.Metro/issues/2852).

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,18 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Data;
 
 namespace ToggleSwitchIsCheckedBindingLoss
 {
 	public partial class MainWindow : Window
 	{
 		public MainWindow() => InitializeComponent();
+
+		void ToggleSwitch_Checked(object sender, RoutedEventArgs e)
+		{
+			var binding = new Binding(nameof(MainWindowViewModel.P2)) { Mode = BindingMode.OneWay };
+			((ToggleSwitch)sender).SetBinding(ToggleSwitch.IsCheckedProperty, binding);
+		}
 	}
 }
